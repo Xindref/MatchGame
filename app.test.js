225c0d7fd548b3ced1a50973649e06c9ee10f203
Dako <dakotahmccrary@gmail.com>
2023-03-13 17:55:52 -0500

    Added Jasmine Standalone

--- app.test.js
+++ app.test.js
@@ -1,6 +1,6 @@
 it('should calculate low-bracket', function () {
     expect(calculateTaxes(10000)).toEqual(1500);
-    expect(calculateTaxes(20000)).toEqual(2000);
+    expect(calculateTaxes(20000)).toEqual(5000);
 });
 
 it('should calculate high-bracket', function () {
